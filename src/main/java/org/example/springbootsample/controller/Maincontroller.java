384f51fadae01b0d92102a43d376f5a5a6e31c87
yugandhar reddy <reddyyugandhar163@gmail.com>
2026-03-23 13:14:24 +0530

    first commit

---
+++ src/main/java/org/example/springbootsample/controller/Maincontroller.java
@@ -0,0 +1,14 @@
+package org.example.springbootsample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Maincontroller {
+
+    @GetMapping("/")
+    public String sample(){
+        return "Application is up...";
+    }
+
+}
