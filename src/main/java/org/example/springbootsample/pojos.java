468f089b5c99fbc257929b49ea1262b302613745
yugandhar reddy <reddyyugandhar163@gmail.com>
2026-03-25 15:56:31 +0530

    iam a pojo class from the yogi side

---
+++ src/main/java/org/example/springbootsample/pojos.java
@@ -0,0 +1,32 @@
+package org.example.springbootsample;
+
+public class pojos {
+    private String animal;
+
+    public String getLions() {
+        return lions;
+    }
+
+    public void setLions(String lions) {
+        this.lions = lions;
+    }
+
+    public String getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(String animal) {
+        this.animal = animal;
+    }
+
+    public String getTiger() {
+        return tiger;
+    }
+
+    public void setTiger(String tiger) {
+        this.tiger = tiger;
+    }
+
+    private  String lions;
+    private  String tiger;
+}
