57d30a53d398edb20cc7e289b8513da0dfe740c7
yugandhar reddy <reddyyugandhar163@gmail.com>
2026-03-25 15:33:25 +0530

    these changes from vinay

--- src/main/java/org/example/springbootsample/controller/Maincontroller.java
+++ src/main/java/org/example/springbootsample/controller/Maincontroller.java
@@ -9,7 +9,9 @@ public class Maincontroller {
     @GetMapping("/")
     public String sample(){
         return "Applicatio my be any fristmer job +second times or third time changes  +my changes are at 5th times bro "
-                +"iam be not noted but 7th changes";
+
+                +"iam be not noted but 6th changes"+"may more changes applied at all stages";
+
     }
 
 }
